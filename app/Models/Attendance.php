f7397c3b100dd73b6ad2dca43dafb2c96faeb3b4
mantosh pandey <mantoshp47@gmail.com>
2025-10-01 18:41:30 +0530

    Changes455

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -41,92 +41,38 @@ class Attendance extends Model
         return "{$hours}h {$minutes}m";
     }
 
-    // public function getLateTimeAttribute()
-    // {
-    //     if (!$this->check_in_at) {
-    //         return "0h 0m 0s";
-    //     }
-    //     $checkIn = Carbon::parse($this->check_in_at);
-    //     $shiftStart = $this->work_date
-    //         ? Carbon::parse($this->work_date)->setTime(10, 0, 0)
-    //         : $checkIn->copy()->setTime(10, 0, 0);
-    //     if ($checkIn->lte($shiftStart)) {
-    //         return "0h 0m 0s";
-    //     }
-    //     $lateSeconds = $shiftStart->diffInSeconds($checkIn);
-    //     return TimeHelper::formatSeconds($lateSeconds);
-    // }
-
-    // public function getOvertimeAttribute()
-    // {
-    //     if (!$this->check_in_at || !$this->check_out_at) {
-    //         return '0h 0m';
-    //     }
-    //     $checkIn = Carbon::parse($this->check_in_at);
-    //     $checkOut = Carbon::parse($this->check_out_at);
-    //     $workedMinutes = $checkIn->diffInMinutes($checkOut);
-    //     // Standard shift = 8 hours (480 minutes)
-    //     $shiftDuration = 480;
-    //     if ($workedMinutes > $shiftDuration) {
-    //         $overtime = $workedMinutes - $shiftDuration;
-    //         return sprintf("%dh %dm", floor($overtime / 60), $overtime % 60);
-    //     }
-    //     return '0h 0m';
-    // }
-
     public function getLateTimeAttribute()
-{
-    if (!$this->check_in_at) {
-        return "0h 0m 0s";
+    {
+        if (!$this->check_in_at) {
+            return "0h 0m 0s";
+        }
+        $checkIn = Carbon::parse($this->check_in_at);
+        $shiftStart = $this->work_date
+            ? Carbon::parse($this->work_date)->setTime(10, 0, 0)
+            : $checkIn->copy()->setTime(10, 0, 0);
+        if ($checkIn->lte($shiftStart)) {
+            return "0h 0m 0s";
+        }
+        $lateSeconds = $shiftStart->diffInSeconds($checkIn);
+        return TimeHelper::formatSeconds($lateSeconds);
     }
 
-    $checkIn = Carbon::parse($this->check_in_at);
-
-    // Shift start time (example: 10:00 AM)
-    $shiftStart = $this->work_date
-        ? Carbon::parse($this->work_date)->setTime(10, 0, 0)
-        : $checkIn->copy()->setTime(10, 0, 0);
-
-    if ($checkIn->lte($shiftStart)) {
-        return "0h 0m 0s";
-    }
-
-    $lateSeconds = $shiftStart->diffInSeconds($checkIn);
-
-    return TimeHelper::formatSeconds($lateSeconds);
-}
-
-public function getOvertimeAttribute()
-{
-    if (!$this->check_in_at || !$this->check_out_at) {
+    public function getOvertimeAttribute()
+    {
+        if (!$this->check_in_at || !$this->check_out_at) {
+            return '0h 0m';
+        }
+        $checkIn = Carbon::parse($this->check_in_at);
+        $checkOut = Carbon::parse($this->check_out_at);
+        $workedMinutes = $checkIn->diffInMinutes($checkOut);
+        // Standard shift = 8 hours (480 minutes)
+        $shiftDuration = 480;
+        if ($workedMinutes > $shiftDuration) {
+            $overtime = $workedMinutes - $shiftDuration;
+            return sprintf("%dh %dm", floor($overtime / 60), $overtime % 60);
+        }
         return '0h 0m';
     }
-
-    $checkIn = Carbon::parse($this->check_in_at);
-    $checkOut = Carbon::parse($this->check_out_at);
-
-    // Total worked minutes
-    $workedMinutes = $checkIn->diffInMinutes($checkOut);
-
-    // Shift duration in minutes (8 hours = 480)
-    $shiftDuration = 480;
-
-    // Late minutes
-    $lateMinutes = 0;
-    if ($checkIn->gt(Carbon::parse($this->work_date)->setTime(10, 0, 0))) {
-        $lateMinutes = Carbon::parse($this->work_date)->setTime(10, 0, 0)->diffInMinutes($checkIn);
-    }
-
-    // Deduct late minutes from worked minutes for overtime calculation
-    $effectiveWorkedMinutes = $workedMinutes - $lateMinutes;
-
-    if ($effectiveWorkedMinutes > $shiftDuration) {
-        $overtime = $effectiveWorkedMinutes - $shiftDuration;
-        return sprintf("%dh %dm", floor($overtime / 60), $overtime % 60);
-    }
-
-    return '0h 0m';
-}
 
     public static function getTotalLateTime($userId)
     {
